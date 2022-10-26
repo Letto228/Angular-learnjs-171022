964c72ccb8d85ddb2ad02a1eeb611b523705ceef
dmitryvokhmin <boxdima1@gmail.com>
2022-10-26 19:21:44 -0400

    Homework 2

--- src/app/modules/products-list/products-list.component.ts
+++ src/app/modules/products-list/products-list.component.ts
@@ -1,8 +1,16 @@
 import { Component } from '@angular/core';
+import { productMock } from '../../shared/products/product.mock';
 
 @Component({
 	selector: 'app-products-list',
 	templateUrl: './products-list.component.html',
 	styleUrls: ['./products-list.component.less'],
 })
-export class ProductsListComponent {}
+export class ProductsListComponent {
+	readonly card = productMock;
+
+  onAddToCard(event: Event) {
+    event.stopPropagation();
+    console.log('Click');
+  }
+}
